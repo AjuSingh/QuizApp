dc04d6a942b3d2a41657ed70b8a7a4784eab4ef9
Ajvinder Singh <aju90534@gmail.com>
2022-07-23 11:06:33 +0530

    :zap: trivia app completed

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 function Main() {
   let [ques, setQues] = useState({});
